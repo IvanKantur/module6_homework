79898c5fc9512e9bba32d11b9508ccf70ea69e97
Ivan Kantur <ivankantur87@gmail.com>
2022-10-25 17:41:59 +0300

    main: add 3d task

---
+++ task3.js
@@ -0,0 +1,12 @@
+function firstNumber(a){
+    return function multiple(b){
+        return a * b
+        }
+}
+
+console.log(firstNumber(7)(5));
+
+
+// Можно через переменную
+// const result = firstNumber(5)( 6);
+// console.log(result);
